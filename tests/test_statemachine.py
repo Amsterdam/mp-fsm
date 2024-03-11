8de10cdf7701d660f732f2fca2d465477c2e4f29
Youri Westerman <y.westerman@amsterdam.nl>
2024-03-11 15:33:55 +0100

    Test example of re-usable callbacks

--- tests/test_statemachine.py
+++ tests/test_statemachine.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 from enum import StrEnum
-from typing import override
+from typing import TypeVar, override
 
 import pytest
 from pytest_mock import MockerFixture
@@ -112,3 +112,65 @@ async def test_statemachine_failing_guard() -> None:
 
     with pytest.raises(GuardException):
         await state_machine.transition(my_object, "my_transition")
+
+
+@pytest.mark.asyncio
+async def test_statemachine_reusable_callbacks(mocker: MockerFixture) -> None:
+    T = TypeVar("T", bound=StateAware)
+
+    class ReusableGuard(BaseGuard[T]):
+        async def __call__(self, obj: T) -> bool:
+            return True
+
+    class MyGuard(ReusableGuard[MyObject]): ...
+
+    guard_spy = mocker.spy(MyGuard, "__call__")
+    guard = MyGuard()
+
+    class ReusableCallback(BaseCallback[T]):
+        async def __call__(self, obj: T) -> None:
+            print("HELLO")
+
+    class MyCallback(ReusableCallback[MyObject]): ...
+
+    callback_spy = mocker.spy(MyCallback, "__call__")
+    callback = MyCallback()
+
+    class MyTransition(BaseTransition[MyObject]):
+        @property
+        def from_states(self) -> list[str]:
+            return [MyStates.START]
+
+        @property
+        def to_state(self) -> str:
+            return MyStates.STOP
+
+        @property
+        @override
+        def guards(self) -> list[BaseGuard[MyObject]]:
+            return [guard]
+
+        @property
+        @override
+        def before(self) -> list[BaseCallback[MyObject]]:
+            return [callback]
+
+        @property
+        @override
+        def after(self) -> list[BaseCallback[MyObject]]:
+            return [callback]
+
+    class MyStateMachine(BaseStateMachine[MyObject]):
+        @property
+        def _transitions(self) -> dict[str, BaseTransition[MyObject]]:
+            return {"my_transition": MyTransition()}
+
+    my_object = MyObject()
+    my_object.state = MyStates.START
+
+    state_machine = MyStateMachine()
+    await state_machine.transition(my_object, "my_transition")
+
+    assert my_object.state == MyStates.STOP
+    assert guard_spy.call_count == 1
+    assert callback_spy.call_count == 2
